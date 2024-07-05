f65503862d920250aaab11a28995610428f86582
R3nn4 <joao.rwt@gmail.com>
2024-07-05 18:05:39 -0300

    Verificando um Número, Transformações em uma String e Funções Matriciais

--- FuncoesDeAltaOrdem.js
+++ FuncoesDeAltaOrdem.js
@@ -1,6 +1,6 @@
 // ------- Ordenação -------
 
-function criterio(i, n){
+function criterioOrdenacao(i, n){
     console.log(i , n);
     //insira aqui o criterio desejado para a ordenação
     if(i > n){
@@ -31,7 +31,7 @@ function ordena(vetor, tamVetor, criterio){
 }
 
 var vetor = [1,9,3,4,5,6,7,8,2];
-ordena(vetor, vetor.length, criterio);
+ordena(vetor, vetor.length, criterioOrdenacao);
 console.log(vetor);
 
 // ------- Criptografia -------
@@ -70,3 +70,86 @@ console.log(string);
 
 string = encripta(string, chave, cifra);
 console.log(string);
+
+// ------- Verificando um Número -------
+
+function criterioVerificacao(num){
+    if(num % 2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function verifica(num, criterio){
+    if(criterio(num) == true){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+var numero = 12;
+console.log(verifica(numero, criterioVerificacao));
+
+// ------- Transformações em uma String -------
+
+function transformacao(char){
+    char = char.toLowerCase();
+    
+    if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u"){
+        char = char.toUpperCase();
+    }
+    else{
+        char = char.toLowerCase();
+    }
+    return char;
+}
+
+function transformaString(string, transformacao){
+    let i;
+    let stringAux = [];
+    for(i = 0; i < string.length; i++){
+        stringAux[i] = transformacao(string[i]);
+    }
+    return stringAux.join("");
+}
+
+var string = "Coltec";
+var stringNova = transformaString(string, transformacao);
+console.log(stringNova);
+
+// ------- Funcoes Matriciais -------
+
+function funcaoMatricial(numLinhas, numColunas){
+    let i;
+    let j;
+    let matriz = [];
+
+    for(i = 0; i < numLinhas; i++){
+        let linha = [];
+
+        for(j = 0; j < numColunas; j++){
+            linha[j] = i + j; //inserir aqui a lei da função matricial;
+            console.log("Linha " + i + ", Coluna " + j + " = " + linha[j]);
+        }
+        
+        matriz[i] = linha;
+    }
+}
+
+function criaMatriz(numMatrizes, funcaoMatricial){
+    let i;
+    let matrizDeMatrizes = [];
+    for(i = 0; i < numMatrizes; i++){
+        console.log("Matriz " + i);
+        matrizDeMatrizes[i] = funcaoMatricial(i+1, i+1);
+    }
+
+    return matrizDeMatrizes;
+}
+
+var numMatrizes = 5;
+criaMatriz(numMatrizes, funcaoMatricial);
